2260066ad25c81b8f5c8bdc49bedf0c582d6fe39
Benedikt Sauer <filmor@gmail.com>
2011-11-03 23:15:21 +1000

    First try of an event handler.

--- event_handler.hpp
+++ event_handler.hpp
@@ -1,17 +1,24 @@
-#ifndef LECTOR_EVENT_HANDLER_HPP
-#define LECTOR_EVENT_HANDLER_HPP
-
-#include "gles_drawer.hpp"
+#ifndef LECTOR_SDL_EVENT_HANDLER_HPP
+#define LECTOR_SDL_EVENT_HANDLER_HPP
 
 namespace lector
 {
 
+    // Handles raw mouse events and emits gesture-events from them
     class event_handler
     {
     public:
-        void 
+        event_handler(lector::scene& scene);
 
-    }
+        void set_current_time(std::size_t time);
+        void handle_up(uint8_t index, std::size_t x, std::size_t y);
+        void handle_down(uint8_t index, std::size_t x, std::size_t y);
+        void handle_motion(uint8_t index, int xrel, int yrel);
+
+    private:
+        std::size_t time_;
+        lector::scene& scene_;
+    };
 
 }
 
